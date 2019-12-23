af754f9f522f3a06e0fa7ad828e9c6404fc4d7d3
Zhang Yong <zhangyong_yz@hotmail.com>
2019-12-23 22:24:31 +0800

    new URL, view and tempate to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -26,7 +26,7 @@ class HomePageTest(TestCase):
         response = self.client.post('/',data={'item_text':'A new list item'})
 
         self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/')
+        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
     
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
@@ -36,10 +36,15 @@ class HomePageTest(TestCase):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('itemey 1',response.content.decode())
-        self.assertIn('itemey 2',response.content.decode())
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+class ListViewTest(TestCase):
+    def test_use_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
